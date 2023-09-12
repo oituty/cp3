72c139154007605ab934261c73b1fc8bf919796b
tutyrodrigues <34607596+tutyrodrigues@users.noreply.github.com>
2023-09-12 15:03:48 -0300

    Button: Adicionada variant outlinedError

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,48 +1,62 @@
 import styled from 'styled-components';
 
-
 const ButtonStyle = styled.button`
-color: #0046C0;
-text-align: center;
-font-family: Open Sans;
-font-size: 16px;
-font-style: normal;
-font-weight: 700;
-line-height: 20px; /* 125% */
-letter-spacing: 0.4px;
-display: flex;
-padding: 6px 13.03px 6px 13px;
-align-items: flex-start;
-border-radius: 4px;
-cursor: pointer;
+  text-align: center;
+  font-family: Open Sans;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 20px;
+  letter-spacing: 0.4px;
+  display: flex;
+  padding: 6px 16px 6px 16px;
+  align-items: flex-start;
+  border-radius: 4px;
+  cursor: pointer;
 
-  &:hover {
-    background-color: #0046C0;
-  }
+
 `;
 
 const ElevatedButton = styled(ButtonStyle)`
+  color: #0046c0;
   box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+  &:hover {
+    background-color: #0046c0;
+  }
 `;
 
 const OutlinedButton = styled(ButtonStyle)`
   background-color: transparent;
-  border: 1px solid #0046C0;
-  color: #0046C0;
+  border: 1px solid #0046c0;
+  color: #0046c0;
 
   &:hover {
-    background-color: #0046C0;
+    background-color: #0046c0;
     color: #fff;
   }
 `;
 
+const OutlinedErrorButton = styled(ButtonStyle)`
+  background-color: transparent;
+  border: 1px solid #fd6157;
+  color: #fd6157;
+
+  &:hover {
+    background-color: #fd6157;
+    color: #fff;
+    border: 1px solid #fd6157;
+
+  }
+`;
+
+const ButtonStyleByVariant = {
+  elevated: ElevatedButton,
+  outlined: OutlinedButton,
+  outlinedError: OutlinedErrorButton,
+};
+
 export default function Button({ onClick, label, variant }) {
-  const RenderedButton =
-    variant === 'elevated'
-      ? ElevatedButton
-      : variant === 'outlined'
-      ? OutlinedButton
-      : ButtonStyle;
+  const RenderedButton = ButtonStyleByVariant[variant];
 
   return (
     <RenderedButton as="button" onClick={onClick}>
