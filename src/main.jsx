54ce771f258d4d7f9ac5b36fcdd82f1d0e8c6174
tutyrodrigues <34607596+tutyrodrigues@users.noreply.github.com>
2023-09-12 12:47:29 -0300

    Vários: Base do projeto react+vite

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import Home from './views/Home';
+import Login from './views/Login';
+import Survey from './views/Survey';
+import NotFound from './views/NotFound';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    errorElement: <NotFound />,
+    element: <App />, 
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/login', element: <Login /> },
+      {
+        path: '/vistoria/:step',
+        element: <Survey />,
+      },
+      { path: '*', element: <NotFound /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
